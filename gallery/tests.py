112a8d3626aed54ea828ac8c92f4f03b27bfc81b
Berthedusabeyuzu <berthe1030@gmail.com>
2019-03-16 11:23:35 +0200

    test

--- gallery/tests.py
+++ gallery/tests.py
@@ -3,47 +3,81 @@ from .models import Location,Category,Image
 # Create your tests here.
 
 
-class LocationTestClass(TestCase):
 
-    # Set up method
-    def setUp(self):
-        self.image= Location(photo_name = 'photo', description ='Good photo', location ='profile',category='sorpt')
-    
-    #Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.photo,Location))
-
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.image,Location))
-    
-     # Testing Save Method
-    def test_save_method(self):
-        self.photo.save_location()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations) > 0)
-    
-    def test_update_method(self):
-        self.update_location()
-        locations = Location.objects.all()
-        self.assertTrue(len(locations) > 0)
-
-class ImageTestClass(TestCase):
-
-    def setUp(self):
-        self.photo = Location(photo_location = 'Image')
-        self.photo.save_location()
-        
-        # Creating a new category and saving it
-        self.new_category = categories(name = 'testing')
-        self.new_category.save()
-
-        self.new_image= Image(title = 'Test Image',post = 'This is a random test Post',location = self.photo)
-        self.new_image.save()
-
-        self.new_image.categories.add(self.new_category)
-
+def setUp(self):
+          self.cat=Category(category='sport')
+          self.location=Location(photo_location='Rwanda')
+          self.image=Image(image='jpg',name='CACO',description="has a good shape",location=self.location,category=self.cat)
+          
     def tearDown(self):
+        Category.objects.all().delete()
         Location.objects.all().delete()
-        category.objects.all().delete()
         Image.objects.all().delete()
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+        self.assertTrue(isinstance(self.location,Location))
+        self.assertTrue(isinstance(self.cat,Category))
+
+    def test_save_image(self):
+        self.cat.save()
+        self.location.save()
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.cat.save()
+        self.location.save()
+        self.image.save_image()
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_get_image_by_id(self):
+        image1 = Image.get_image(id=self.image.id)
+        self.assertEqual(self.image, image1)
+
+    def test_get_images_by_category(self):
+        images=Image.get_category_images(cat=self.cat)
+        imagess=Image.objects.filter(category=self.cat)
+        self.assertQuerysetEqual(images,imagess)
+
+    def test_filter_by_location(self):
+        images=Image.get_location_images(loc=self.location.pk)
+        imagess=Image.objects.filter(location=self.location)
+        self.assertQuerysetEqual(images,imagess)
+
+    def test_update_image(self):
+        new_image=Image.update_name(1,'bebe')
+        self.assertTrue(self.image.name != new_image)
+
+    def test_save_location(self):
+        self.location.save_location()
+        locations=Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+    def test_save_category(self):
+        self.cat.save_category()
+        categories=Category.objects.all()
+        self.assertTrue(len(categories)>0)
+        
+    def test_delete_location(self):
+    
+        self.location.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
+
+    def test_delete_category(self):
+    
+        self.cat.delete_category()
+        categories= Category.objects.all()
+        self.assertTrue(len(categories) == 0)
+
+    def test_update_location(self):
+        new_location=Location.update_all(1,'bebe','nana','nan')
+        self.assertTrue(self.location != new_location)
+
+    def test_update_category(self):
+        new_category=Category.update_name(1,'bebe')
+        self.assertTrue(self.cat.category != new_category)
